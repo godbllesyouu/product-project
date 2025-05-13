a817c52f3fb1203c8b08e9a672ba31929e27127c
Aziz <azizsmoyan19@gmail.com>
2025-05-13 22:41:04 +0300

    uodate

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+
+  const [data, setData] = useState([])
+  const [searchTerm, setSearchTerm] = useState("")
+  useEffect(() => {
+    fetch('https://dummyjson.com/products')
+      .then(res => res.json())
+      .then(results => setData(results.products));
+  }, []);
+  const filtredProducts = data.filter((product) => product.title.toLowerCase().includes(searchTerm)); 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="product-list">
+        <div className='inp'>
+        <input type='text' placeholder='search' value={searchTerm} onChange={(e) => setSearchTerm(e.target.value)}
+        className='search-input' />
+       </div>
+       <div className='title-card'>
+        {filtredProducts.map((product) => (
+          <div className="product-card" key={product.id}>
+            <img src={product.thumbnail}
+              alt={product.title}
+              className="product-image" />
+            <div className="prod-title">
+              <h3 href='ej.html'>{product.title}</h3>
+              <p>${product.price}</p>
+            </div>
+            
+          </div>
+        ))}
+      </div>
+      </div>
+      
     </div>
-  );
+  )
 }
 
 export default App;
